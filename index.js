c31da743146d841197fb39c9dcafd9b3555b2dff
Margherita <145118725+Margherita97@users.noreply.github.com>
2025-06-02 13:10:50 +0200

    Add sound

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const buttonSelector = document.querySelectorAll(".drum");
 
 for (const element of buttonSelector) {
     element.addEventListener("click", function() {
-        alert("I got clicked!");
+        const audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
 }
 
